847e9917ef691ab486fd0c7afd04c599bd2d353a
vindrayulian <vindrayulian@gmail.com>
2024-05-29 02:26:51 +0700

    new desain

--- asset/javascript/content/main.js
+++ asset/javascript/content/main.js
@@ -1,50 +1,16 @@
-// toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-x');
-    navbar.classList.toggle('active');
-};
-
-// Scroll nav active
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-
-    // sticky navbar
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    // remove icon and navbar
-    menuIcon.classList.remove('fa-x');
-    navbar.classList.remove('active');
-};
-
-// scroll reveal
-ScrollReveal({
-    reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
+$(window).on("scroll", function() {
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop >= 100){
+        $('body').addClass('fixed-header');
+    } else{
+        $('body').removeClass('fixed-header');
+    }
 });
 
-ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
-ScrollReveal().reveal('.home-img, .service-container, .portofolio-box, .contact form', {origin: 'bottom'});
-ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
-ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
+// $(document).ready(function() {
+//     new Typed('#type-it', {
+//         strings: ['FrontEnd Developer'],
+//         typeSpeed: 100,
+//         loop: true
+//     });
+// });
